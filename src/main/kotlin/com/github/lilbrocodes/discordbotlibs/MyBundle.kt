1ac794e0e64cc7beb8d2b576528a04453c80a873
GitHub Action <action@github.com>
2023-12-28 10:04:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/lilbrocodes/discordbotlibs/MyBundle.kt
+++ src/main/kotlin/com/github/lilbrocodes/discordbotlibs/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lilbrocodes.discordbotlibs
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lilbrocodes/discordbotlibs/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lilbrocodes/discordbotlibs/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lilbrocodes.discordbotlibs.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
